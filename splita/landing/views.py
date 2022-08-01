47e957a99303d33201f358e3c2a673337a21d7d6
Hydrophoenix <nkwochaugo2006@gmail.com>
2022-08-01 23:23:09 +0100

    Addition of signup html and css
    Added signup page and css to the landing app

--- splita/landing/views.py
+++ splita/landing/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+
+def landing(request):
+    template = loader.get_template('landing.html')
+    return HttpResponse(template.render())
