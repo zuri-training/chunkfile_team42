fb7ad2ecf2ae0b9ca420aa37d3b8a80c07924c84
Hydrophoenix <105215970+Hydrophoenix@users.noreply.github.com>
2022-08-01 23:49:41 +0100

    Addition of signup page and css file (#7)
    * Addition of signup html and css
    
    Added signup page and css to the landing app
    
    * Update signup.html

--- splita/splita/settings.py
+++ splita/splita/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'landing',
+    'users',
+    'prototype',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'images/'
+
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, 'static'),
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
