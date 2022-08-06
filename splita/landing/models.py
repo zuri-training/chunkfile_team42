e06effa2774eca3e36e7ff30b325418731c04aab
Eke Kalu <ekendukwe@gmail.com>
2022-08-06 07:51:53 -0300

    user authentication

--- splita/landing/models.py
+++ splita/landing/models.py
@@ -1,3 +1,45 @@
+from distutils.command.upload import upload
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.core import validators
 
-# Create your models here.
+class customuserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields) 
+
+class customuser(AbstractBaseUser, PermissionsMixin):
+    username=None
+    id = models.BigAutoField(primary_key = True,)
+    email = models.EmailField(validators = [validators.EmailValidator()], unique=True, max_length = 200)
+    fullname = models.CharField(max_length=50, blank=True)
+    password = models.CharField( max_length=30, blank=True)
+    profilepic = models.ImageField(null=True, blank=True, upload_to='image/')
+
+    objects = customuserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['password', 'fullname']
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
--- splita/landing/views.py
+++ splita/landing/views.py
@@ -1,21 +1,43 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import redirect, render
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from . models import customuser
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
-
-def index(request):
-    return HttpResponse("E work")
-
+##test
+@login_required(login_url='/login')
 def landing(request):
-    template = loader.get_template('landing.html')
-    return HttpResponse(template.render())
-
-
-def login_view(request):
-    return render(request, 'login.html')
-
+    return render(request, 'landing/landing.html')
+##endoftest
 
 def signup(request):
-    template = loader.get_template('signup.html')
-    return HttpResponse(template.render())
+    if request.method == "POST":
+        fullname = request.POST['fullname']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        if customuser.objects.filter(email=email):
+            messages.error(request, "Email Already Exists")
+            return redirect('/signup')
+
+        newuser = customuser.objects._create_user(email, password, fullname)
+        newuser.save()
+        
+        messages.success(request, "Your account has been successfully created")
+        return redirect('/login')
+     
+    return render(request, 'landing/signup.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/landing')
+        else:
+            messages.error(request, 'Email or Password does not exist')
+    else:
+        return render(request, 'landing/login.html')
+
