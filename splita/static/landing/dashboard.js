5d115819b390ace44a64eec06ae6ce8ff899eeeb
olatomide007 <olatomideafolabi@gmail.com>
2022-08-08 01:27:10 +0100

    file authentication ready

--- splita/static/landing/dashboard.js
+++ splita/static/landing/dashboard.js
@@ -1,11 +1,3 @@
-let upload = () => {
-    let progressValue = document.querySelector('.progress-value'),
-    progress = document.querySelector('.progress')
-    progressValue.setAttribute('id', 'play-animation')
-    
-    progress.style.opacity = 1
-}
-
 
 // FOR DROP DOWN
 document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
@@ -13,13 +5,34 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
   
     dropZoneElement.addEventListener("click", (e) => {
       inputElement.click();
+
+     
     });
   
     inputElement.addEventListener("change", (e) => {
       if (inputElement.files.length) {
         updateThumbnail(dropZoneElement, inputElement.files[0]);
       }
+
+      let progressBar = document.querySelector('.progress_bar'),
+      fileAuthen = document.querySelector('.file_authen2'),
+      chunkBtn = document.querySelector('.chunk_btn'),
+      leadLink = document.querySelector('.lead_link')
+
+      var a = 0;
+      var run = setInterval(frames, 50);
+      function frames() {
+        a = a + 1;
+        if (a == 101) {
+          clearInterval(run);
+          progressBar.style.display= 'none',
+          fileAuthen.style.display = 'block',
+          chunkBtn.setAttribute('class', 'btn_authen'),
+          leadLink.href = "./fileAuthen3.html"
+        }
+      }
     });
+  
   
     dropZoneElement.addEventListener("dragover", (e) => {
       e.preventDefault();
@@ -33,6 +46,7 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
     });
   
     dropZoneElement.addEventListener("drop", (e) => {
+
       e.preventDefault();
   
       if (e.dataTransfer.files.length) {
@@ -41,6 +55,24 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
       }
   
       dropZoneElement.classList.remove("drop-zone--over");
+
+      // SHOW FILE AUTHENTICATION
+
+      let progressBar = document.querySelector('.progress_bar');
+      fileAuthen = document.querySelector('.file_authen2'),
+      chunkBtn = document.querySelector('.chunk_btn')
+
+      var a = 0;
+      var run = setInterval(frames, 50);
+      function frames() {
+        a = a + 1;
+        if (a == 101) {
+          clearInterval(run);
+          progressBar.style.display = 'none';
+          fileAuthen.style.display = 'block'
+          chunkBtn.setAttribute('class', 'btn_authen')
+        }
+      }
     });
   });
   
@@ -52,11 +84,13 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
     progress = document.querySelector('.progress')
     progressValue.setAttribute('id', 'play-animation')
     
-    progress.style.opacity = 1
+    
+    progress.style.display = 'block';
 
     let upload_text = document.querySelector('.upload_text')
 
-    upload_text.style.opacity = 1
+
+    upload_text.style.display = 'block'
 
 
     let thumbnailElement = dropZoneElement.querySelector(".progress-value", ".upload_text");
@@ -65,6 +99,7 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
     if (dropZoneElement.querySelector(".break_line_content")) {
       dropZoneElement.querySelector(".break_line_content").remove();
     }
+
 
   
     // First time - uploading and progress bar made visible
@@ -76,3 +111,7 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
   
     thumbnailElement.dataset.label = file.name;
 }
+
+
+
+
