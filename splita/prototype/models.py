f96a1f1faf3375dcf474cb992fad9ec14a985046
Hydrophoenix <nkwochaugo2006@gmail.com>
2022-08-11 12:48:19 +0100

    Update models.py

--- splita/prototype/models.py
+++ splita/prototype/models.py
@@ -1,3 +1,12 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+
+
+class Document(models.Model):
+    docfile_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    docfile = models.FileField(upload_to='documents/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    #user = models.ForeignKey(user)
+
