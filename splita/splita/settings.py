ebcf20b2f435a1190410962846affc15d3764d2b
Hydrophoenix <nkwochaugo2006@gmail.com>
2022-08-11 12:50:43 +0100

    Update settings.py

--- splita/splita/settings.py
+++ splita/splita/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'landing.apps.LandingConfig',
+    'prototype.apps.PrototypeConfig',
     'users',
     'prototype',
 ]
@@ -125,7 +126,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-AUTH_USER_MODEL = 'landing.customuser' 
+AUTH_USER_MODEL = 'landing.customuser'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
