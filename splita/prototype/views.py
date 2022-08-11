f2c6004e880724956a1bc8c7866e815ef6ec9502
Hydrophoenix <nkwochaugo2006@gmail.com>
2022-08-11 12:50:24 +0100

    Update views.py

--- splita/prototype/views.py
+++ splita/prototype/views.py
@@ -1,48 +1,70 @@
-from pyexpat.errors import messages
+import csv
+import json
+import pandas as pd
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from zipfile import ZipFile
-import pandas as pd
+import zipfile
+from django.template import loader
 import os
-import uuid
+from .models import Document
 
 
 # Create your views here.
+@login_required(login_url='/login')
+def dashboard(request):
+    return render(request, "prototype/dashboard.html")
+
+
 def splita(request):
     if request.method == "POST":
+        # this is the data in the file
+        file_settings = request.FILES['selectedfile']
+        # this is the file name extracted from the file and should include the file extension
+        file_name = file_settings.name
+        # getting out the file extension from the file name
+        file_ext = file_name.split('.')[1]
+        # this is the chunk size as provided by the user
+        chunk_size = int(request.POST['chunk_row_size'])
+        #user = request.user
 
-        file_settings = request.FILES['file']
-        file_name = request.POST['file_name']
-        if file_name == "":
-            file_name = file_settings.name
-        chunk_size = int(request.POST['chunk_size'])
-        user = request.user
+       # if chunk_size <= 0 or file_settings is None:
+        #messages.error(request, "Fields cannot be empty!")
+        # return redirect('prototype/dashboard.html')
 
-        if chunk_size == "" or file_settings is None:
-            messages.error(request, "Fields cannot be empty!")
-            return redirect('/')
-
-        if file_settings.endswith(".csv", ".json"):
+        if file_ext in ("csv", "json"):
+            data_set = pd.read_csv(file_settings, chunksize=chunk_size)
             counter = 0
-            for chunks in pd.read_csv(file_settings, chunksize=chunk_size):
-                with ZipFile(f"media/{file_name}-.zip", 'a') as zip:
-                    file_name = f"{file_name}-" + str(counter) + ".csv"
-                    zip.write(file_name,chunks.to_csv(file_name, index=False))
+
+            for chunk in data_set:
+                with zipfile.ZipFile(f"media/{file_name}.zip", 'a') as zip_file:
+                    file_name = file_name + str(counter) + f'.{file_ext}'
+                    zip_file.write(chunk.to_csv(file_name, index=False))
                 os.remove(file_name)
                 counter += 1
 
-            csv_file = File.objects.create(user=user, file=f"{user}{file_name}-.zip")
+            csv_file = Document.objects.create(docfile=f"{file_name}.zip")
             csv_file.save()
 
             messages.info(request, "Split completed")
-            return redirect('/storage')
+            return render(request, 'prototype/storage.html')
 
-        messages.error(request, "Invalid file format, Please upload a valid csv or json file")
-        return redirect('/dashboard')
+        messages.error(
+            request, "Invalid file format, Please upload a valid csv or json file")
+        return redirect('prototype/dashboard.html')
 
-    return render(request, 'dashboard.html')
+    return render(request, 'prototype/dashboard.html')
 
 
-
-
+def storage(request):
+    template = loader.get_template('prototype/storage.html')
+    return HttpResponse(template.render())
+    # return render(request, 'prototype/storage.html')
+    #mydocuments = Document.objects.all().values()
+    #template = loader.get_template('storage.html')
+    # context = {
+    # 'mydocuments' : mydocuments,
+    # }
+    # return HttpResponse(template.render(context, request))
+    # return render(request, (output))
