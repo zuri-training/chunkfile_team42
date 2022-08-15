5c30e087031da565b602dde8fe090754ffd22212
teymie <65744646+teymie@users.noreply.github.com>
2022-08-15 06:38:39 -0700

    Previous (#147)
    * download option working
    
    * fixed the download option added the file page
    
    * delete function
    
    * updated
    
    Co-authored-by: Hydrophoenix <105215970+Hydrophoenix@users.noreply.github.com>

--- splita/prototype/views.py
+++ splita/prototype/views.py
@@ -1,19 +1,24 @@
+from datetime import datetime
 import os
+from urllib import response
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 import zipfile
 from django.template import loader
+
 from .models import Documents
 import pandas as pd
 from os import listdir
 from os.path import isfile, join
-from django.conf import settings
+# from django.conf import settings
 from django.views.generic.base import TemplateView
 from pathlib import Path
 from .models import Documents
 import mimetypes
+from prototype import settings
+from django.http import FileResponse 
 
 
 # Create your views here.
@@ -26,9 +31,16 @@ def fileAuthen3(request):
     print(documents)
     return render(request, 'prototype/fileAuthen3.html',{'files': documents})
 
-def filepage(request):
-    return render(request, 'prototype/filepage.html')
+def myfile(request):
+    documents = Documents.objects.filter(user=request.user)
+    print(documents)
+    return render(request, 'prototype/filepage.html',{'files': documents})
 
+def delete(request):
+    filename = request.GET.get('filename')
+    documents = Documents.objects.filter(user=request.user,docfile=filename )
+    documents.delete()
+    return redirect('prototype:myfile')
 
 def splita(request):
     if request.method == "POST":
@@ -39,6 +51,7 @@ def splita(request):
         # getting out the file extension from the file name
         file_ext = file_settings_name.split('.')[1]
         file_name = file_settings_name.split('.')[0]
+        zip_name = file_name + str(datetime.utcnow().timestamp()).split('.')[0]
         # this is the chunk size as provided by the user
         chunk_size = int(request.POST['chunk_row_size'])
         #file_size = Path(file_settings).stat()
@@ -58,34 +71,39 @@ def splita(request):
                 for chunk in data_set:
                     file_name1 = file_name + str(counter) + f'.{file_ext}'
                     file = chunk.to_csv(file_name1, index=False)
-                    with zipfile.ZipFile(f"media/{file_name}.zip", 'a', compression=zipfile.ZIP_DEFLATED) as zip_file:
+                    with zipfile.ZipFile(f"media/{zip_name}.zip", 'a', compression=zipfile.ZIP_DEFLATED) as zip_file:
                         zip_file.write(file_name1,file)
                     os.remove(file_name1)
                     counter += 1
 
-                resulting_file = Documents.objects.create(docfile=f"{file_name1}.zip",user=request.user, totalchunk=counter+1)
+                resulting_file = Documents.objects.create(docfile=f"{zip_name}.zip",user=request.user, totalchunk=counter+1)
                 resulting_file.save()
 
                 messages.info(request, "Split completed")
                 return redirect('prototype:fileAuthen3')
 
             if file_ext == "json":
-                data_set = pd.read_json(file_settings, lines = True, chunksize=chunk_size)
+
+                print('file',file_settings)
+                data_set = pd.read_json(file_settings, lines = True)
+
                 counter = 0
 
                 for chunk in data_set:
-                    file_name1 = file_name + str(counter) + f'.{file_ext}'
-                    file = chunk.to_json(file_name1, index=1, orient = 'record')
-                    with zipfile.ZipFile(f"media/{file_name}.zip", 'a', compression=zipfile.ZIP_DEFLATED) as zip_file:
-                        zip_file.write(file_name1,file)
-                    os.remove(file_name1)
+                    file_name = file_name + str(counter) + f'.{file_ext}'
+                    file = chunk.to_json(file_name, index=1, orient = 'record')
+                    with zipfile.ZipFile(f"media/{zip_name}.zip", 'a', compression=zipfile.ZIP_DEFLATED) as zip_file:
+                        zip_file.write(file_name,file)
+                    os.remove(file_name)
                     counter += 1
 
-                resulting_file = Documents.objects.create(docfile=f"{file_name}.zip",user=request.user, totalchunk=counter+1)
+                resulting_file = Documents.objects.create(docfile=f"{zip_name}.zip",user=request.user, totalchunk=counter+1)
                 resulting_file.save()
 
                 messages.info(request, "Split completed")
-                return redirect('prototype:fileAuthen3')
+
+                return redirect(request, 'prototype:fileAuthen3')
+
 
         messages.error(
             request, "Invalid file format, Please upload a valid csv or json file")
@@ -94,16 +112,27 @@ def splita(request):
     return render(request, 'prototype/dashboard2.html')
 
 
-# def download_file(request):
-#     # fill these variables with real values
-#     fl_path = '/media/{file_name1}.zip'
-#     filename = 'downloaded_file_name.extension'
+def download_file(request):
+    # fill these variables with real values
+    filename = request.GET.get('filename')
+    print('settings', os.path.join(settings.MEDIA_ROOT[0],filename))
+    fl_path = os.path.join(settings.MEDIA_ROOT[0],filename)
 
-#     fl = open(fl_path, 'r')
-#     mime_type, _ = mimetypes.guess_type(fl_path)
-#     response = HttpResponse(fl, content_type=mime_type)
-#     response['Content-Disposition'] = "attachment; filename=%s" % filename
-#     return response
+
+    fl = open(fl_path, 'rb')
+    print('fl', fl)
+    mime_type, _ = mimetypes.guess_type(fl_path)
+    print('mimetype', mime_type)
+    response = FileResponse(fl, content_type=mime_type)
+    response['Content-Disposition'] = "attachment; filename=%s" % filename
+    return response
+        # filename = 'downloaded_file_name.extension'
+
+    # fl = open(fl_path, 'r')
+    # mime_type, _ = mimetypes.guess_type(fl_path)
+    # response = HttpResponse(fl, content_type=mime_type)
+    # response['Content-Disposition'] = "attachment; filename=%s" % filename
+    # return response
 
 
 #def storage(request):
