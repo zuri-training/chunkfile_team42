4e5c2ce6669041d93c4ee77b7b73a7efc6a080bf
teymie <65744646+teymie@users.noreply.github.com>
2022-08-16 05:48:31 -0700

    update settings

--- splita/splita/settings.py
+++ splita/splita/settings.py
@@ -120,7 +120,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -130,7 +129,6 @@ EMAIL_HOST_PASSWORD = 'jfsxzptmmmbrrdxv'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media'),
@@ -144,7 +142,10 @@ AUTH_USER_MODEL = 'landing.customuser'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "static/"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 import dj_database_url
 db=dj_database_url.config(conn_max_age=500)
