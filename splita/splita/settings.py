14c39f426b9e98ef57151b36d64f89c80801daa4
teymie <65744646+teymie@users.noreply.github.com>
2022-08-16 04:45:44 -0700

    update

--- splita/splita/settings.py
+++ splita/splita/settings.py
@@ -130,10 +130,10 @@ EMAIL_HOST_PASSWORD = 'jfsxzptmmmbrrdxv'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-STATIC_ROOT = (os.path.join(BASE_DIR,'staticfiles'))
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
 
-MEDIA_ROOT = BASE_DIR/ 'media',
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media'),
 
 
 AUTH_USER_MODEL = 'landing.customuser'
